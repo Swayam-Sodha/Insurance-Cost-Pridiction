b023ceac663c01a9ac4ce7235ef4b024cdddd111
Swayam-Sodha <swayam.sodha@outlook.com>
2025-09-13 15:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,31 +15,21 @@ st.title("💊 Medical Insurance Cost Predictor")
 st.subheader("By Swayam Sodha")
 
 age = st.number_input("Age", min_value=1, max_value=100, value=30)
-# sex = st.selectbox("Sex", ["male", "female"])
-sex = st.radio(
-    "Sex",
-    ["Male", "Female"]
-)
+sex = st.radio("Sex", ["male", "female"])
 bmi = st.number_input("BMI", min_value=10.0, max_value=60.0, value=25.0)
 children = st.number_input("Children", min_value=0, max_value=10, value=0)
-# smoker = st.selectbox("Smoker", ["yes", "no"])
-smoker = st.radio(
-    "Smoker",
-    [":green[Yes]", ":red[No]"]
-)
+smoker = st.radio("Smoker", ["yes", "no"])
 region = st.selectbox("Region", ["northeast", "southeast", "northwest", "southwest"])
-sex = sex.lower()
-smoker = smoker.lower()
 
 if st.button("Predict"):
     sample = pd.DataFrame([{
-        "age": age, "sex": sex, "bmi": bmi,
-        "children": children, "smoker": smoker, "region": region
+        "age": age,
+        "sex": sex,
+        "bmi": bmi,
+        "children": children,
+        "smoker": smoker,
+        "region": region
     }])
     sample_prepared = preprocessor.transform(sample)
     prediction = model.predict(sample_prepared)[0]
     st.success(f"Estimated Insurance Cost: ${prediction:,.2f}")
-
-
-
-
